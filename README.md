364068b3d0dc8b29d898b140b0c5f71691c9214e
Rossella Manzo <Rossella Manzo>
2015-12-30 20:28:38 +0100

    Ho aggiunto due frociate

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 
 EatOnline Progetto
 ##################
+
+
+
+
+
+Guida GIT
+
+1. Aggiungere i file allo Stack: 
+
+git add --all
+
+2. Commit dei file
+
+
--- index.php
+++ index.php
@@ -15,8 +15,7 @@
         $CHome->impostaPagina();
         
         error_reporting(E_ALL); 
-        ini_set('display_errors', 1);
-        
+
         ?>
     </body>
 </html>
