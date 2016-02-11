dfaf8cbe6dce8e31526bf75664b5d59865a53613
Rossella Manzo <Rossella Manzo>
2016-02-11 21:52:08 +0100

    js3

--- js/main.js
+++ js/main.js
@@ -30,14 +30,12 @@ $(document).ready(function () {
             var quantita_element = $("#prodotto-" + id).find("td.quantita");
             var quantita = quantita_element.text();
             quantita++;
-            
             var str_prezzo_element = $("#prodotto-" + id).find("td.prezzo");
             var str_prezzo = str_prezzo_element.text();
             var str_prezzo_array = str_prezzo.split(" ");
             var prezzo = str_prezzo_array[1];
             var str_subTot_element = $("#prodotto-" + id).find("td.subTotale");
-            subTot = prezzo*quantita;
-            
+            subTot = prezzo * quantita;
             quantita_element.text(quantita);
             str_subTot_element.text("Sub Totale: " + subTot + " \u20ac");
             animationGrow(quantita_element);
@@ -51,7 +49,14 @@ $(document).ready(function () {
             var quantita = quantita_element.text();
             quantita--;
             if (quantita > 0) {
+                var str_prezzo_element = $("#prodotto-" + id).find("td.prezzo");
+                var str_prezzo = str_prezzo_element.text();
+                var str_prezzo_array = str_prezzo.split(" ");
+                var prezzo = str_prezzo_array[1];
+                var str_subTot_element = $("#prodotto-" + id).find("td.subTotale");
+                subTot = prezzo * quantita;
                 quantita_element.text(quantita);
+                str_subTot_element.text("Sub Totale: " + subTot + " \u20ac");
                 animationGrow(quantita_element);
             } else {
                 $("#prodotto-" + id).hide();
@@ -70,8 +75,8 @@ $(document).ready(function () {
         ajax1("carrello", "aggiungi." + id, function () {
             var nome_element = $("#item-" + id).find("p.nome");
             var nome = nome_element.text();
-            var w = Math.floor((screen.availWidth)/4);
-            var h = Math.floor((screen.availHeight)/2);
+            var w = Math.floor((screen.availWidth) / 4);
+            var h = Math.floor((screen.availHeight) / 2);
             var myWindow = window.open("", "", "width = 580, height = 80, top = " + h + ",left = " + w);
             var content = ("<title>EatOnline</title><body bgcolor = #550000><p align = center><font size = 6px color = #ffffff>Il prodotto \'" + nome + "\' è stato aggiunto al carrello!</font></p></body>");
             myWindow.document.write(content);
